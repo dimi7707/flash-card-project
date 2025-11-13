64c1a612aedc40f92c58f6359723e6bfec5a131a
Dimitri Avila <dimitriavila@MacBook-Air-de-Dimitri.local>
2025-11-12 21:21:18 -0500

    Fix: prisma import

--- app/api/cards/route.ts
+++ app/api/cards/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import prisma from '@/lib/prisma';
+import { prisma } from '@/lib/prisma';
 import { validateLength } from '@/lib/utils';
 import { Prisma } from '@prisma/client';
 
