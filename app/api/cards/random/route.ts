30b5c1c7c92cffffd172678bb08104c9875f26e1
Dimitri Avila <dimitriavila@MacBook-Air-de-Dimitri.local>
2025-11-12 21:06:22 -0500

    Fix: adding dynamic route specification to deploy in vercel

--- app/api/cards/random/route.ts
+++ app/api/cards/random/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 
+export const dynamic = 'force-dynamic'
+
 /**
  * GET /api/cards/random
  * Fetch 15 random cards for testing
